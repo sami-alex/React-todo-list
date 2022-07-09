9d485d6967866206b05f97209697dc1b5888c2ab
samuel alemayehu <samuel.alemyehu13@gmail.com>
2022-07-09 10:00:49 +0300

    my first commit

---
+++ src/components/todo.js
@@ -0,0 +1,121 @@
+// import React from "react";
+// export default function ToDO(){
+
+// const [data,setData]=React.useState({task:""})
+// const [tasks,setTask]=React.useState([])
+
+// function AddTask()
+// {
+// setTask([...tasks,data.task])
+
+
+// }
+
+// function DeleteTask(x){
+//    let copiedTask=[...tasks]
+//     copiedTask.splice(x,1)
+//     setTask(copiedTask)
+
+// }
+// function handleInput(event){
+
+//     const {name,value}=event.target
+//    setData(prevstate=>({...prevstate,
+//     [name]:[value]}
+   
+//    )
+    
+//     )
+    
+        
+//     }
+// return(
+// <>
+// <input
+// placeholder="Add task"
+// value={data.task}
+// onChange={handleInput}
+// name="task"
+
+// />
+// <button onClick={AddTask}>AddTask</button>
+
+// {
+//  tasks.map((item,index)=>(
+//     <p key={index} >
+        
+//         {item}
+//         <button onClick={()=>DeleteTask(index)}>delete</button>
+
+//     </p>
+//  ))
+// }
+
+
+// </>
+
+// )
+
+
+// }
+import React from "react";
+export default function ToDO(){
+
+const [data,setData]=React.useState("")
+const [tasks,setTask]=React.useState([])
+
+function AddTask()
+{
+    
+setTask([...tasks,data])
+setData('')
+
+}
+
+function DeleteTask(x){
+   let copiedTask=[...tasks]
+    copiedTask.splice(x,1)
+    setTask(copiedTask)
+
+}
+function handleInput(event){
+
+   
+   setData(event.target.value)
+
+console.log(event.target)
+    
+    
+    
+        
+    }
+return(
+<>
+<input
+placeholder="Add task"
+value={data}
+onChange={handleInput}
+name="task"
+
+/>
+<button onClick={AddTask}>AddTask</button>
+
+{
+
+tasks.length?(tasks.map((item,index)=>(
+    <p key={index} >
+        
+        {item}
+        <button onClick={()=>DeleteTask(index)}>delete</button>
+
+    </p>
+ )) ):<p>You dont have any tasks</p>
+}
+
+
+</>
+
+)
+
+
+}
